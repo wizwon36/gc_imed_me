ec7d4fee943a8d6bb038527a0ac79c3aac766270
wizwon36 <wizwon36@gmail.com>
2026-03-24 16:24:16 +0900

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   APP_NAME: '녹십자아이메드 의료장비 관리시스템',
-  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbws0qOQz4wpjxElKVeIQnr49-htbPfw1UM-MmqLhOubXBFTYJ_BkPndAMjX05uWVKAN/exec',
+  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbwErrsjsL4MntM_NwjRVed9AKJoGLF57UIx1K-v-OfAt0sfwkmB6xuyiVK2QH9s7BFP/exec',
   SITE_BASE_URL: 'https://wizwon36.github.io/gc_imed_me'
 };
