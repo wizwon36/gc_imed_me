80bd6deb25eb1e7b2fd21a6d4167fa928e0b3e2a
wizwon36 <wizwon36@gmail.com>
2026-04-17 13:55:15 +0900

    Update config.js

--- assets/js/core/config.js
+++ assets/js/core/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   APP_NAME: 'MSO관리팀 업무지원 시스템',
-  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbxYw8DXcYCdrGWGlO9JWCIPjXPaO3SeYb-otwtC9M5qb7Z8lf7YOUgo0NV9kNbT2cSE/exec',
+  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbyIBGpxsOcVyA_z8J2jkq8Ak8vf4rByvQFdKMI92rLWLPSIIv5G8O93jVhsCWFGM-Ya/exec',
   SITE_BASE_URL: 'https://wizwon36.github.io/gc_imed_me'
 };
