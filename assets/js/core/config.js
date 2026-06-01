bc1ab44e3756905381572e0830f0ea3987b9d51f
wizwon36 <wizwon36@gmail.com>
2026-06-01 15:00:11 +0900

    Update config.js

--- assets/js/core/config.js
+++ assets/js/core/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   APP_NAME: 'MSO관리팀 업무지원 시스템',
-  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbwof_j2ydqiaZuXLyHV4NkawOqgS5XWAEb39aQmiBjAvJo7_9sIhqCD_V91vCfy7wo/exec',
+  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbyq679x3l5nSm2HYH_3vExOcctueRJGQmfN4I01VvtNlhs6-BOJ4Ms4ownYiXPSvVPq/exec',
   SITE_BASE_URL: 'https://wizwon36.github.io/gc_imed_me',
 
   // 의료장비 앱 오픈 허용 의원 목록 — 신규 의원 추가 시 여기만 수정
